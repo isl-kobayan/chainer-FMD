7c32e1f1116d0ab4b62896758813ccc17327b8df
isl-kobayan <adpa0179@chiba-u.jp>
2016-03-25 20:00:26 +0900

    Added files via upload

---
+++ models/__init__.py
@@ -0,0 +1,21 @@
+import nin
+import alex
+import alexbn
+import googlenet
+import googlenet2
+import googlenetbn
+import caffealex
+import caffegooglenet
+import VGGNet
+import chainerVGGNet
+
+NIN = nin.NIN
+Alex = alex.Alex
+AlexBN = alexbn.AlexBN
+GoogLeNet = googlenet.GoogLeNet
+GoogLeNet2 = googlenet2.GoogLeNet2
+GoogLeNetBN = googlenetbn.GoogLeNetBN
+CaffeAlex = caffealex.CaffeAlex
+CaffeGoogLeNet = caffegooglenet.CaffeGoogLeNet
+VGGNet = VGGNet.VGGNet
+ChainerVGGNet = chainerVGGNet.ChainerVGGNet
---
+++ models/caffealex.py
@@ -0,0 +1,45 @@
+import chainer
+import chainer.functions as F
+from chainer.functions import caffe
+import sys
+
+class CaffeAlex(caffe.CaffeFunction):
+
+    insize = 224
+
+    def __init__(self):
+        self.labelsize = 10
+        print ('loading caffemodel ...')
+        super(CaffeAlex, self).__init__('./models/bvlc_alexnet.caffemodel')
+        print ('Done.')
+        super(CaffeAlex, self).add_link('fc8fmd', F.Linear(4096, self.labelsize))
+        self.train = True
+
+    def __call__(self, x, t):
+        h = F.max_pooling_2d(F.local_response_normalization(F.relu(self.conv1(x)), n=5), 3, stride=2)
+        h = F.max_pooling_2d(F.local_response_normalization(F.relu(self.conv2(h)), n=5), 3, stride=2)
+        h = F.relu(self.conv3(h))
+        h = F.relu(self.conv4(h))
+        h = F.max_pooling_2d(F.relu(self.conv5(h)), 3, stride=2)
+        h = F.dropout(F.relu(self.fc6(h)), train=self.train)
+        h = F.dropout(F.relu(self.fc7(h)), train=self.train)
+        h = self.fc8fmd(h)
+        self.loss = F.softmax_cross_entropy(h, t)
+        self.accuracy = F.accuracy(h, t)
+        return self.loss
+
+    def test(self, x, t):
+        h = F.max_pooling_2d(F.local_response_normalization(F.relu(self.conv1(x)), n=5), 3, stride=2)
+        h = F.max_pooling_2d(F.local_response_normalization(F.relu(self.conv2(h)), n=5), 3, stride=2)
+        h = F.relu(self.conv3(h))
+        h = F.relu(self.conv4(h))
+        h = F.max_pooling_2d(F.relu(self.conv5(h)), 3, stride=2)
+        h = F.dropout(F.relu(self.fc6(h)), train=self.train)
+        h = F.dropout(F.relu(self.fc7(h)), train=self.train)
+        h = self.fc8fmd(h)
+        self.loss = F.softmax_cross_entropy(h, t)
+        self.accuracy = F.accuracy(h, t)
+        self.lastacts = h
+        return self.loss
+
+
