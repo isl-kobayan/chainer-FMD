ff0b83e4e9ce4f6aac278a84bc3b6f7d31dde4da
isl-kobayan <adpa0179@chiba-u.jp>
2016-03-25 22:01:21 +0900

    rename sh

--- make_list.sh
+++ make_list.sh
@@ -3,27 +3,29 @@
 # delete file if exists
 function clearf() {
   if [ -e $1 ]; then
-    if [ -f $1 ] ; then
-      rm $1
-    elif [ -d $1 ] ; then
-      rm -r $1
-    fi
+    rm $1
   fi
 }
 
-img_l="256"
-if [ $# -ge 1 ]; then
-  img_l=$1
-fi
+
+filespath="./files.txt"
+labelspath="./labels.txt"
+imagelistpath="./image_list.txt"
+num2labelpath="./num2label.txt"
+
+clearf $filespath
+clearf $labelspath
+clearf $imagelistpath
+clearf $num2labelpath
+
 
 in_dir="./FMD/image"
-out_dir=$in_dir$img_l
+if [ $# -ge 1 ]; then
+  in_dir=$1
+fi
+
 files="${in_dir}/*"
 dirary=()
-
-#rm -r $out_dir
-clearf $out_dir
-mkdir $out_dir
 
 for filepath in $files; do
   if [ -d $filepath ] ; then
@@ -34,14 +36,15 @@ done
 labelnum=0
 #echo "ディレクトリ一覧"
 for d in ${dirary[@]}; do
-  mkdir "${out_dir}/${d##*/}"
+  echo ${d##*/} 1>> $num2labelpath
   m_files="${d}/*"
   for m_file in $m_files; do
     if [ -f $m_file ] ; then
       if [[ "$m_file" =~ ^.*\.(jpg|png|bmp)$ ]]; then
-        #scale image
-        convert -crop 384x384+64+0 -geometry "${img_l}x${img_l}" $m_file "${out_dir}/${d##*/}/${m_file##*/}"
-	#echo "$img_l"
+        #fileary+=("$filepath")
+        #echo $m_file 1>> $filespath
+        #echo $labelnum 1>> $labelspath
+	echo -e "${m_file}\t${labelnum}" 1>> $imagelistpath
       fi
     fi
   done
